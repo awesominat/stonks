3b262da9d986779293b5f9f6d21712594e6a6f4d
quiz3 <113859565+quiz3@users.noreply.github.com>
2023-10-30 20:47:10 -0400

    API access interface and Data Access drivers have been implemented

---
+++ src/drivers/Finnhub.java
@@ -0,0 +1,267 @@
+package drivers;
+
+import entities.CompanyInformation;
+import entities.CompanyNews;
+import entities.PricePoint;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import use_cases.APIAccessInterface;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO
+// code the code using okhttp to necessary information about the stocks
+public class Finnhub implements APIAccessInterface {
+    String APIKEY;  // Question: should this be private or static or final or some combination of these things?
+    int NUM_ARTICLES = 5;
+    String RESOLUTION = "D";
+
+    public Finnhub() {
+        File f = new File("file.txt");
+        if(!f.exists()) {
+            // do something
+            System.out.println("please create a file in the src parent directory called file.txt with your Finnhub token");
+            // Question: should we be throwing FileNotFoundException here?
+            return;
+        }
+        FileReader fileReader = new FileReader();
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(f);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        // Set constant attribute `APIKEY` of Finnhub object to your API key
+        APIKEY = fileReader.readFromInputStream(inputStream);
+    }
+
+    @Override
+    public boolean isMarketOpen() {
+        String url = "https://finnhub.io/api/v1/stock/market-status?exchange=US&token=" + APIKEY;
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            assert response.body() != null;
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            return (Boolean) responseBody.get("isOpen");
+        }
+        catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public CompanyInformation getCompanyProfile(String ticker) {
+        // Get profile for company `ticker`.
+
+        // Define url for API call
+        String url = "https://finnhub.io/api/v1/stock/profile2?symbol=" + ticker + "&token=" + APIKEY;
+
+        // Use OkHttp client to access API
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try {
+            // Make API call
+            Response response = client.newCall(request).execute();
+            assert response.body() != null;
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            // Save API response values
+            String country = (String) responseBody.get("country");
+            String name = (String) responseBody.get("name");
+            String ticker_response = (String) responseBody.get("ticker");
+            String weburl = (String) responseBody.get("weburl");
+            String ipo = (String) responseBody.get("ipo");
+
+            // Create and return CompanyInformation object with information from API response
+            return new CompanyInformation(country, name, ticker_response, weburl, ipo);
+        }
+        catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<CompanyNews> getCompanyNews(String ticker, LocalDate from, LocalDate to) {
+        // Get news related to company `ticker` from the last month.
+        // Limit it to the first `NUM_ARTICLES` articles.
+
+        // LocalDate documentation: https://docs.oracle.com/javase/8/docs/api/java/time/LocalDate.html
+        // Finnhub documentation: https://finnhub.io/docs/api/company-news
+        // Date section of url: &from=2023-08-15&to=2023-08-20 (not unix)
+
+        String url = "https://finnhub.io/api/v1/company-news?symbol=" + ticker + "&from=" + from + "&to=" + to +
+                "&token=" + APIKEY;
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            assert response.body() != null;
+
+            // Read API data into a JSON array
+            JSONArray responseBody = new JSONArray(response.body().string());
+            // Initialize output array (for return value)
+            List<CompanyNews> ret = new ArrayList<CompanyNews>();
+
+            // Iterate through dictionary of recent news articles
+            // for (int i = 0; i < responseBody.length(); i ++) {
+            for (int i = 0; i < NUM_ARTICLES; i ++) {
+                // Get current article from JSON array
+                Object curr_article = responseBody.get(i);
+                // Create JSON Object for current article
+                JSONObject responseArticle = new JSONObject(curr_article.toString());
+
+                // Convert API response into usable variables
+                String category = (String) responseArticle.get("category");
+                String headline = (String) responseArticle.get("headline");
+                String url_out = (String) responseArticle.get("url");
+                String summary = (String) responseArticle.get("summary");
+                Integer timeStamp = (Integer) responseArticle.get("datetime");  // get timeStamp from API response
+
+                // Convert timeStamp from Integer to LocalDate
+                Instant instant = Instant.ofEpochSecond(timeStamp.longValue());
+                ZonedDateTime zdt = instant.atZone(ZoneId.systemDefault());
+                LocalDate datetime = zdt.toLocalDate();
+
+                ret.add(new CompanyNews(category, datetime, headline, url_out, summary));
+            }
+            return ret;
+        }
+        catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public PricePoint getCurrentPrice(String ticker) {
+        String url = "https://finnhub.io/api/v1/quote?symbol=" + ticker + "&token=" + APIKEY;
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            assert response.body() != null;
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            // TODO
+            // very important to know if we want information like
+            //d
+            //Change
+            //dp
+            //Percent change
+            //h
+            //High price of the day
+            //l
+            //Low price of the day
+            //o
+            //Open price of the day
+            //pc
+            //Previous close price
+            LocalDate now = LocalDate.now();
+            Double result;
+
+            if (responseBody.get("c") instanceof BigDecimal) {
+                result = ((BigDecimal) responseBody.get("c")).doubleValue();
+            } else {
+                result = (Double) responseBody.get("c");
+            }
+
+            return new PricePoint(now, result);
+        }
+        catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<PricePoint> getLastMonthPrices(String ticker) {
+        ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
+        Instant instantNow = now.toInstant();
+        long unixTimeNow = instantNow.getEpochSecond();
+        ZonedDateTime oneMonthAgo = now.minusMonths(1);
+        Instant instantOneMonthAgo = oneMonthAgo.toInstant();
+        long unixTimeOneMonthAgo = instantOneMonthAgo.getEpochSecond();
+
+        String url = "https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=" + RESOLUTION + "&from=" + unixTimeOneMonthAgo + "&to=" + unixTimeNow + "&token=" + APIKEY;
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            assert response.body() != null;
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            JSONArray openPrices = responseBody.getJSONArray("o");
+            JSONArray timeStamps = responseBody.getJSONArray("t");
+            List<PricePoint> ret = new ArrayList<PricePoint>();
+
+            for (int i = 0; i < openPrices.length(); i ++) {
+                Object curTimeStamp = timeStamps.get(i);
+                Integer intTimeStamp = (Integer) curTimeStamp;
+                Long longTimeStamp = intTimeStamp.longValue();
+
+                Instant instant = Instant.ofEpochSecond(((Integer) timeStamps.get(i)).longValue());
+                ZonedDateTime zdt = instant.atZone(ZoneId.systemDefault());
+                LocalDate localDate = zdt.toLocalDate();
+
+                Object priceObj = openPrices.get(i);
+                double priceValue;
+
+                if (priceObj instanceof BigDecimal) {
+                    priceValue = ((BigDecimal) priceObj).doubleValue();
+                } else if (priceObj instanceof Integer) {
+                    priceValue = (double) (int) priceObj;
+                } else {
+                    throw new IllegalArgumentException("Unsupported type in openPrices: " + priceObj.getClass());
+                }
+                ret.add(new PricePoint(localDate, priceValue));
+            }
+            return ret;
+        }
+        catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
