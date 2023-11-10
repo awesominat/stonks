5c235c82a37038d9a01b86335859e48071a4d317
Zain <zainhmoustafa@gmail.com>
2023-11-10 13:27:20 -0500

    added Mockito and JUnit4 unit tests for buy and sell

--- src/interface_adapters/ResetBalance/ResetBalancePresenter.java
+++ src/interface_adapters/ResetBalance/ResetBalancePresenter.java
@@ -3,6 +3,7 @@ package interface_adapters.ResetBalance;
 import interface_adapters.ViewManagerModel;
 import use_cases.ResetBalance.ResetBalanceOutputData;
 import view.ResetBalanceView;
+
 public class ResetBalancePresenter implements ResetBalanceOutputBoundary{
     private final ResetBalanceView resetBalanceViewModel;
     private ViewManagerModel viewManagerModel;
