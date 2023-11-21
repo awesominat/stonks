24ee6528ff53591c6f5e828078469cf88ee935f2
Ricky Grosvenor-Pramanick <ronakpramanick@gmail.com>
2023-11-20 20:36:48 -0500

    Rebased the conflicting issues

--- src/main/java/interface_adapters/Dashboard/DashboardPresenter.java
+++ src/main/java/interface_adapters/Dashboard/DashboardPresenter.java
@@ -5,28 +5,20 @@ import use_cases.Dashboard.DashboardOutputData;
 
 public class DashboardPresenter implements DashboardOutputBoundary {
 
-    private final DashboardViewModel sellViewModel;
     private final DashboardViewModel dashboardViewModel;
     private final ViewManagerModel viewManagerModel;
 
-    public DashboardPresenter(ViewManagerModel viewManagerModel,
-                              DashboardViewModel sellViewModel, DashboardViewModel dashboardViewModel) {
+    public DashboardPresenter(ViewManagerModel viewManagerModel, DashboardViewModel dashboardViewModel) {
         this.viewManagerModel = viewManagerModel;
-        this.sellViewModel = sellViewModel;
         this.dashboardViewModel = dashboardViewModel;
     }
 
     @Override
-    public void prepareSuccessView(DashboardOutputData response) {
+    public void prepareSuccessView(DashboardOutputData dashboardOutputData) {
+        DashboardState dashboardState = dashboardViewModel.getState();
         dashboardViewModel.firePropertyChanged();
         viewManagerModel.setActiveView(dashboardViewModel.getViewName());
         viewManagerModel.firePropertyChanged();
     }
 
-    @Override
-    public void prepareFailView(String error) {
-        DashboardState sellState = sellViewModel.getState();
-        sellState.setAmountError(error);
-        sellViewModel.firePropertyChanged();
-    }
 }
--- src/main/java/interface_adapters/Dashboard/DashboardState.java
+++ src/main/java/interface_adapters/Dashboard/DashboardState.java
@@ -1,18 +1,34 @@
 package interface_adapters.Dashboard;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class DashboardState {
-    private String amountError = null;
+    private HashMap<String, Float> userStats;
+    // change owned stocks data type
+    private HashMap<String, List> ownedStocks;
 
-    public DashboardState(String amountError) {
-        this.amountError = amountError;
+    public DashboardState(HashMap<String, Float> userStats, HashMap<String, List> ownedStocks) {
+        this.userStats = userStats;
+        this.ownedStocks = ownedStocks;
     }
 
-    public String getAmountError() {
-        return amountError;
+    public HashMap<String, List> getOwnedStocks() {
+        return this.ownedStocks;
     }
 
-    public void setAmountError(String amountError) {
-        this.amountError = amountError;
+    public HashMap<String, Float> getUserStats() {
+        return this.userStats;
+    }
+
+    public void setUserStats(HashMap<String, Float> userStats) {
+        this.userStats = userStats;
+    }
+
+    public void setOwnedStocks(HashMap<String, List> ownedStocks) {
+        this.ownedStocks = ownedStocks;
     }
 
     // Because of the previous copy constructor, the default constructor must be explicit. Hence overloading.
