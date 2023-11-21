1636002747c0967602e0f34ee032078cfdcd4815
Ricky Grosvenor-Pramanick <ronakpramanick@gmail.com>
2023-11-20 20:40:26 -0500

    Rebased DashboardOutputBoundary

--- src/main/java/interface_adapters/Dashboard/DashboardOutputBoundary.java
+++ src/main/java/interface_adapters/Dashboard/DashboardOutputBoundary.java
@@ -3,6 +3,10 @@ package interface_adapters.Dashboard;
 import use_cases.Dashboard.DashboardOutputData;
 
 public interface DashboardOutputBoundary {
+<<<<<<< HEAD
     void prepareSuccessView(DashboardOutputData dashboardOutputData);
 
+=======
+    void prepareSuccessView(DashboardOutputData result);
+>>>>>>> d3010d4 (Altered DashboardOutputBoundary)
 }
