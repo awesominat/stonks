bedb78065f1cf6372ed2d10e753c58fbc4042f58
quiz3 <113859565+quiz3@users.noreply.github.com>
2023-11-25 20:13:31 -0500

    many additions/patches completed on GetInfo and GetNews interface adapters

--- src/main/java/interface_adapters/GetInfo/GetInfoPresenter.java
+++ src/main/java/interface_adapters/GetInfo/GetInfoPresenter.java
@@ -1,4 +1,30 @@
 package interface_adapters.GetInfo;
 
-public class GetInfoPresenter {
+import interface_adapters.ViewManagerModel;
+import use_cases.GetInfo.GetInfoOutputBoundary;
+import use_cases.GetInfo.GetInfoOutputData;
+
+public class GetInfoPresenter implements GetInfoOutputBoundary {
+
+    private final GetInfoViewModel getInfoViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public GetInfoPresenter(GetInfoViewModel getInfoViewModel, ViewManagerModel viewManagerModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.getInfoViewModel = getInfoViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(GetInfoOutputData response) {
+        // TODO: Is this supposed to actually display anything upon succeeding or is it a behind-the-scenes use case?
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        // TODO
+        GetInfoState state = getInfoViewModel.getState();
+        state.setTickerError(error);
+        getInfoViewModel.firePropertyChanged();
+    }
+
 }
