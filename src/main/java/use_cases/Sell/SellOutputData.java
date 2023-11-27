e65a547b580389661da000120abe3bc7f99659ef
gursi26 <gursimar.batra@gmail.com>
2023-11-27 11:00:30 -0500

    removed dependency to dashboardstate for updating sell page, now the sell use case can directly make api calls to get updated information. Added more columns to sell table including current sell price for all units and a single unit. Added refresh button to update sell table based on updated stock prices.

--- src/main/java/use_cases/Sell/SellOutputData.java
+++ src/main/java/use_cases/Sell/SellOutputData.java
@@ -1,12 +1,33 @@
 package use_cases.Sell;
 
+import java.util.List;
+
 public class SellOutputData {
     public Double amount;
     public String ticker;
+    public boolean executeTypeSell;
+    public List<String> ownedStocks;
+    public List<Double> ownedAmounts;
+    public List<Double> sellAmounts;
+    public Double balance;
 
     public SellOutputData(Double amount, String ticker) {
         this.amount = amount;
         this.ticker = ticker;
+        this.executeTypeSell = true;
+    }
+
+    public SellOutputData(
+            List<String> ownedStocks,
+            List<Double> ownedAmounts,
+            List<Double> sellAmounts,
+            Double balance
+    ) {
+        this.ownedStocks = ownedStocks;
+        this.ownedAmounts = ownedAmounts;
+        this.sellAmounts = sellAmounts;
+        this.balance = balance;
+        this.executeTypeSell = false;
     }
 
     public Double getAmount() {
@@ -15,5 +36,25 @@ public class SellOutputData {
 
     public String getTicker() {
         return ticker;
+    }
+
+    public boolean isExecuteTypeSell() {
+        return executeTypeSell;
+    }
+
+    public List<Double> getOwnedAmounts() {
+        return ownedAmounts;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public List<String> getOwnedStocks() {
+        return ownedStocks;
+    }
+
+    public List<Double> getSellAmounts() {
+        return sellAmounts;
     }
 }
