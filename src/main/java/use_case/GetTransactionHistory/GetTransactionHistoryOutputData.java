762d2b34c7b17859e0fafc40eb22ca33cac3ed65
Ricky Pramanick <107711090+rickypramanick@users.noreply.github.com>
2023-11-30 22:17:59 -0500

    Refactored code to work with Iterator Design Pattern (#183)

--- src/main/java/use_case/GetTransactionHistory/GetTransactionHistoryOutputData.java
+++ src/main/java/use_case/GetTransactionHistory/GetTransactionHistoryOutputData.java
@@ -4,17 +4,17 @@ import java.util.HashMap;
 import java.util.List;
 
 public class GetTransactionHistoryOutputData {
-    private HashMap<String, List<List<String>>> userRecord;
+    private List<List<String>> userRecord;
 
-    public GetTransactionHistoryOutputData(HashMap<String, List<List<String>>> userRecord) {
+    public GetTransactionHistoryOutputData(List<List<String>> userRecord) {
         this.userRecord = userRecord;
     }
 
-    public void setUserRecord(HashMap<String, List<List<String>>> userRecord) {
+    public void setUserRecord(List<List<String>> userRecord) {
         this.userRecord = userRecord;
     }
 
-    public HashMap<String, List<List<String>>> getUserRecord() {
+    public List<List<String>> getUserRecord() {
         return userRecord;
     }
 }
