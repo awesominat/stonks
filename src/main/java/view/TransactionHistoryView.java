9468368a53890e04e712675a4272c2e7ea5a31e9
gursi26 <gursimar.batra@gmail.com>
2023-12-01 16:29:50 -0500

    fixed bug in transaction history view, restructured screen layout

--- src/main/java/view/TransactionHistoryView.java
+++ src/main/java/view/TransactionHistoryView.java
@@ -17,18 +17,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionHistoryView extends JPanel implements ActionListener, PropertyChangeListener {
-
     public final String viewName = "transactionHistory";
     private final GetTransactionHistoryViewModel getTransactionHistoryViewModel;
-    JTable table;
-    final JButton back;
     GetTransactionHistoryController getTransactionHistoryController;
+
+    final JTable table;
+    final JButton back;
     DefaultComboBoxModel<String> stockInputFieldFilterModel;
-    DefaultComboBoxModel<String> typeInputFieldFilterModel;
     final JComboBox<String> stockInputFieldFilter;
+    DefaultComboBoxModel<String> typeInputFieldFilterModel;
+    final JComboBox<String> typeInputFieldFilter;
+
     String selectedStock = null;
     String selectedType = null;
-    final JComboBox<String> typeInputFieldFilter;
 
     public TransactionHistoryView(
             GetTransactionHistoryController getTransactionHistoryController,
@@ -38,18 +39,19 @@ public class TransactionHistoryView extends JPanel implements ActionListener, Pr
     ) {
 
         this.getTransactionHistoryController = getTransactionHistoryController;
-
         this.getTransactionHistoryViewModel = getTransactionHistoryViewModel;
-
         this.getTransactionHistoryViewModel.addPropertyChangeListener(this);
-
         setPreferredSize(new Dimension(400, 400));
-
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-
+        this.getTransactionHistoryController.execute();
 
         back = new JButton(getTransactionHistoryViewModel.BACK_BUTTON_LABEL);
-        back.setMaximumSize(new Dimension(20, 10));
+        table = new JTable();
+        JLabel filterLabel = new JLabel("Filter settings:");
+        filterLabel.setFont(new Font(filterLabel.getFont().getName(), Font.BOLD, 20));
+        JLabel stockFilterLabel = new JLabel("Stock filter");
+        JLabel typeFilterLabel = new JLabel("Transaction type filter");
+        JScrollPane scrollPane = new JScrollPane(table);
 
         stockInputFieldFilterModel = new DefaultComboBoxModel<>();
         stockInputFieldFilter = new JComboBox<>(stockInputFieldFilterModel);
@@ -57,32 +59,34 @@ public class TransactionHistoryView extends JPanel implements ActionListener, Pr
         typeInputFieldFilterModel = new DefaultComboBoxModel<>();
         typeInputFieldFilter = new JComboBox<>(typeInputFieldFilterModel);
 
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+
+        leftPanel.add(back);
+        leftPanel.add(filterLabel);
+        leftPanel.add(stockFilterLabel);
+        leftPanel.add(stockInputFieldFilter);
+        leftPanel.add(typeFilterLabel);
+        leftPanel.add(typeInputFieldFilter);
+
+        JPanel rightPanel = new JPanel(new BorderLayout());
+        rightPanel.add(scrollPane);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightPanel);
+        splitPane.setResizeWeight(0.05);
+
         back.addActionListener(
                 evt -> {
                     dashboardViewModel.firePropertyChanged();
-
                     viewManagerModel.setActiveView(dashboardViewModel.getViewName());
-
                     viewManagerModel.firePropertyChanged();
                 }
         );
 
-        this.getTransactionHistoryController.execute();
-        table = new JTable();
-
-        JScrollPane scrollPane = new JScrollPane(table);
-
-//        JLabel stockInputFieldFilterTitle = new JLabel("Select an owned stock");
-//        stockInputFieldFilterTitle.setMaximumSize(new Dimension(100,100));
-//        stockInputFieldFilterTitle.setAlignmentX(SwingConstants.SOUTH_WEST);
-
         stockInputFieldFilter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                GetTransactionHistoryState currentState = getTransactionHistoryViewModel.getState();
-
                 selectedStock = String.valueOf(stockInputFieldFilter.getSelectedItem());
-
                 getTransactionHistoryViewModel.firePropertyChanged();
             }
         });
@@ -90,18 +94,16 @@ public class TransactionHistoryView extends JPanel implements ActionListener, Pr
         typeInputFieldFilter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                GetTransactionHistoryState currentState = getTransactionHistoryViewModel.getState();
-
                 selectedType = String.valueOf(typeInputFieldFilter.getSelectedItem());
-
                 getTransactionHistoryViewModel.firePropertyChanged();
             }
         });
+        typeInputFieldFilterModel.addElement("No filter");
+        typeInputFieldFilterModel.addElement("BUY");
+        typeInputFieldFilterModel.addElement("SELL");
+        typeInputFieldFilterModel.addElement("TOPUP");
 
-        this.add(typeInputFieldFilter);
-        this.add(stockInputFieldFilter);
-        this.add(back);
-        add(scrollPane, BorderLayout.CENTER);
+        this.add(splitPane);
     }
 
     public void actionPerformed(ActionEvent evt) {
@@ -112,74 +114,42 @@ public class TransactionHistoryView extends JPanel implements ActionListener, Pr
         getTransactionHistoryController.execute();
 
         stockInputFieldFilterModel.removeAllElements();
-
         stockInputFieldFilterModel.addElement("No filter");
-
         for (String stock: getTransactionHistoryViewModel.getState().allStocksInHistory()){
-
             if (stockInputFieldFilterModel.getIndexOf(stock) == -1 && !stock.equals("RESET")) {
-
                 stockInputFieldFilterModel.addElement(stock);
-
             }
-
         }
-        stockInputFieldFilterModel.setSelectedItem(selectedStock);
-
-        typeInputFieldFilterModel.removeAllElements();
-
-        typeInputFieldFilterModel.addElement("No filter");
-
-        for (String type: getTransactionHistoryViewModel.getState().allTypesInHistory()){
-
-            if (typeInputFieldFilterModel.getIndexOf(type) == -1) {
-
-                typeInputFieldFilterModel.addElement(type);
-
-            }
-
+        if (selectedStock != null) {
+            stockInputFieldFilterModel.setSelectedItem(selectedStock);
         }
 
         typeInputFieldFilterModel.setSelectedItem(selectedType);
 
-
         GetTransactionHistoryState state = getTransactionHistoryViewModel.getState();
 
         DefaultTableModel tableModel = new DefaultTableModel();
-
-
         tableModel.addColumn("Stock");
-
         tableModel.addColumn("Transaction Type");
-
         tableModel.addColumn("Amount");
-
         tableModel.addColumn("Price");
-
         tableModel.addColumn("Date");
-
         List<List<String>> userRecord = new ArrayList<>();
         if (selectedStock == "No filter") {
              userRecord = state.getUserRecord();
         } else if (getTransactionHistoryViewModel.getState().allStocksInHistory().contains(selectedStock)) {
-
             Filter filterForStockName = new FilterByStockName();
-
             userRecord = filterForStockName.filter(state.getUserRecord(), selectedStock);
-
         }
 
         if (selectedType == "No filter") {
             userRecord = state.getUserRecord();
         } else if (getTransactionHistoryViewModel.getState().allTypesInHistory().contains(selectedType)) {
-
             Filter filterForTransactionType = new FilterByTransactionType();
-
             userRecord = filterForTransactionType.filter(state.getUserRecord(), selectedType);
         }
 
         for (List<String> rowData : userRecord) {
-
                 String stock = rowData.get(0);
                 String type = rowData.get(1);
                 String amount = rowData.get(2);
