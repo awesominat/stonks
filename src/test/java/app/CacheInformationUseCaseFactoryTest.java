483ee5cdff0ec88530f9457dca93882eeb8d8e8f
Zain <zainhmoustafa@gmail.com>
2023-12-01 18:59:36 -0500

    removed usecase factory tests

--- src/test/java/app/CacheInformationUseCaseFactoryTest.java
+++ src/test/java/app/CacheInformationUseCaseFactoryTest.java
@@ -1,38 +1,38 @@
-package app;
-
-import interface_adapter.CacheStockInformation.CacheStockInformationController;
-import interface_adapter.CacheStockInformation.CacheStockInformationViewModel;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import use_case.APIAccessInterface;
-import use_case.CacheStockInformation.CacheStockInformationDataAccessInterface;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-class CacheInformationUseCaseFactoryTest {
-
-    @Mock
-    private CacheStockInformationViewModel cacheStockInformationViewModel;
-    @Mock
-    private CacheStockInformationDataAccessInterface fileUserDataAccessObject;
-    @Mock
-    private APIAccessInterface apiAccessInterface;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void testCreateCacheStockInformationUseCase() {
-        CacheStockInformationController controller = CacheInformationUseCaseFactory.createCacheStockInformationUseCase(
-                cacheStockInformationViewModel,
-                fileUserDataAccessObject,
-                apiAccessInterface
-        );
-
-        assertNotNull(controller, "CacheStockInformationController should not be null");
-    }
-}
+//package app;
+//
+//import interface_adapter.CacheStockInformation.CacheStockInformationController;
+//import interface_adapter.CacheStockInformation.CacheStockInformationViewModel;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import use_case.APIAccessInterface;
+//import use_case.CacheStockInformation.CacheStockInformationDataAccessInterface;
+//
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//class CacheInformationUseCaseFactoryTest {
+//
+//    @Mock
+//    private CacheStockInformationViewModel cacheStockInformationViewModel;
+//    @Mock
+//    private CacheStockInformationDataAccessInterface fileUserDataAccessObject;
+//    @Mock
+//    private APIAccessInterface apiAccessInterface;
+//
+//    @BeforeEach
+//    void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//    }
+//
+//    @Test
+//    void testCreateCacheStockInformationUseCase() {
+//        CacheStockInformationController controller = CacheInformationUseCaseFactory.createCacheStockInformationUseCase(
+//                cacheStockInformationViewModel,
+//                fileUserDataAccessObject,
+//                apiAccessInterface
+//        );
+//
+//        assertNotNull(controller, "CacheStockInformationController should not be null");
+//    }
+//}
