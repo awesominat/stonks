9872c284c19e319bcd3ea7a3fe56a308b65e02ea
Ricky Grosvenor-Pramanick <ronakpramanick@gmail.com>
2023-12-01 00:53:59 -0500

    Added Filter by Stock to View

--- src/main/java/view/TransactionHistoryView.java
+++ src/main/java/view/TransactionHistoryView.java
@@ -1,9 +1,7 @@
 package view;
 
 import interface_adapter.Dashboard.DashboardViewModel;
-import interface_adapter.GetTransactionHistory.GetTransactionHistoryController;
-import interface_adapter.GetTransactionHistory.GetTransactionHistoryState;
-import interface_adapter.GetTransactionHistory.GetTransactionHistoryViewModel;
+import interface_adapter.GetTransactionHistory.*;
 import interface_adapter.Sell.SellState;
 import interface_adapter.ViewManagerModel;
 
@@ -26,7 +24,8 @@ public class TransactionHistoryView extends JPanel implements ActionListener, Pr
     JTable table;
     final JButton back;
     GetTransactionHistoryController getTransactionHistoryController;
-    final JComboBox<String> stockInputFieldFilter = new JComboBox<String>();
+    DefaultComboBoxModel<String> stockInputFieldFilterModel;
+    final JComboBox<String> stockInputFieldFilter;
     String selectedStock = null;
     final JComboBox<String> typeInputFieldFilter = new JComboBox<String>();
 
@@ -36,81 +35,56 @@ public class TransactionHistoryView extends JPanel implements ActionListener, Pr
             ViewManagerModel viewManagerModel,
             DashboardViewModel dashboardViewModel
     ) {
+
         this.getTransactionHistoryController = getTransactionHistoryController;
+
         this.getTransactionHistoryViewModel = getTransactionHistoryViewModel;
+
         this.getTransactionHistoryViewModel.addPropertyChangeListener(this);
-
-        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        setLayout(new BorderLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-
-        JPanel topPanel = new JPanel(new GridBagLayout());
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.CENTER;
 
         setPreferredSize(new Dimension(400, 400));
 
-        this.getTransactionHistoryController.execute();
-
-        JLabel title = new JLabel("Transaction History");
-
-        JTableHeader header = new JTableHeader();
-        header.add(title);
-        header.setBackground(Color.LIGHT_GRAY);
-
-        title.setPreferredSize(new Dimension(160, 20));
-        Border border = BorderFactory.createLineBorder(Color.BLACK);
-        title.setBorder(border);
-        title.setText("Transaction History");
-        title.setAlignmentX(SwingConstants.NORTH);
-        title.setFont(new Font("Helvetica", Font.BOLD, 18));
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        setLayout(new BorderLayout());
 
-        topPanel.add(title);
 
         back = new JButton(getTransactionHistoryViewModel.BACK_BUTTON_LABEL);
         back.setMaximumSize(new Dimension(20, 10));
 
+        stockInputFieldFilterModel = new DefaultComboBoxModel<>();
+        stockInputFieldFilter = new JComboBox<>(stockInputFieldFilterModel);
+
         back.addActionListener(
                 evt -> {
                     dashboardViewModel.firePropertyChanged();
+
                     viewManagerModel.setActiveView(dashboardViewModel.getViewName());
+
                     viewManagerModel.firePropertyChanged();
                 }
         );
 
-        topPanel.add(back);
+        this.getTransactionHistoryController.execute();
         table = new JTable();
 
         JScrollPane scrollPane = new JScrollPane(table);
 
-        JLabel stockInputFieldFilterTitle = new JLabel("Select an owned stock");
-        stockInputFieldFilterTitle.setMaximumSize(new Dimension(100,100));
-        stockInputFieldFilterTitle.setAlignmentX(SwingConstants.SOUTH_WEST);
-
-        for (String stock: getTransactionHistoryViewModel.getState().allStocksInHistory()){
-            stockInputFieldFilter.addItem(stock);
-        }
+//        JLabel stockInputFieldFilterTitle = new JLabel("Select an owned stock");
+//        stockInputFieldFilterTitle.setMaximumSize(new Dimension(100,100));
+//        stockInputFieldFilterTitle.setAlignmentX(SwingConstants.SOUTH_WEST);
 
         stockInputFieldFilter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 GetTransactionHistoryState currentState = getTransactionHistoryViewModel.getState();
+
                 selectedStock = String.valueOf(stockInputFieldFilter.getSelectedItem());
+
                 getTransactionHistoryViewModel.firePropertyChanged();
             }
         });
-        topPanel.add(stockInputFieldFilter);
 
-//
-//        this.add(titleLabel);
-        add(topPanel, BorderLayout.NORTH);
-        add(stockInputFieldFilter, BorderLayout.NORTH);
-//        add(title, BorderLayout.NORTH);
-        setLayout(new BorderLayout());
-//        add(back, BorderLayout.BEFORE_FIRST_LINE);
+        this.add(stockInputFieldFilter);
+        this.add(back);
         add(scrollPane, BorderLayout.CENTER);
     }
 
@@ -120,7 +94,21 @@ public class TransactionHistoryView extends JPanel implements ActionListener, Pr
 
     public void propertyChange(PropertyChangeEvent evt) {
         getTransactionHistoryController.execute();
-        System.out.println(selectedStock);
+
+        stockInputFieldFilterModel.removeAllElements();
+
+        stockInputFieldFilterModel.addElement("No filter");
+
+        for (String stock: getTransactionHistoryViewModel.getState().allStocksInHistory()){
+
+            if (stockInputFieldFilterModel.getIndexOf(stock) == -1 && !stock.equals("RESET")) {
+
+                stockInputFieldFilterModel.addElement(stock);
+
+            }
+
+        }
+        stockInputFieldFilterModel.setSelectedItem(selectedStock);
 
         GetTransactionHistoryState state = getTransactionHistoryViewModel.getState();
 
@@ -140,17 +128,43 @@ public class TransactionHistoryView extends JPanel implements ActionListener, Pr
 
         List<List<String>> userRecord = state.getUserRecord();
 
-        for (List<String> rowData : userRecord) {
-            String stock = rowData.get(0);
-            String type = rowData.get(1);
-            String amount = rowData.get(2);
-            String price = rowData.get(3);
-            String date = rowData.get(4);
-            tableModel.addRow(new Object[]{stock,
-                    type, amount, "$" + price, date
+        if (selectedStock == "No filter") {
+            for (List<String> rowData : userRecord) {
+
+                String stock = rowData.get(0);
+                String type = rowData.get(1);
+                String amount = rowData.get(2);
+                String price = rowData.get(3);
+                String date = rowData.get(4);
+                tableModel.addRow(new Object[]{stock,
+                                type, amount, "$" + price, date
+                        }
+                );
             }
-            );
+        } else if (getTransactionHistoryViewModel.getState().allStocksInHistory().contains(selectedStock)) {
+
+            Filter filterForStockName = new FilterByStockName();
+
+            List<List<String>> filteredTransactions = filterForStockName.filter(userRecord, selectedStock);
+
+            for (List<String> rowData : filteredTransactions) {
+
+                if (rowData.get(0) == selectedStock) {
+
+                    String stock = rowData.get(0);
+                    String type = rowData.get(1);
+                    String amount = rowData.get(2);
+                    String price = rowData.get(3);
+                    String date = rowData.get(4);
+
+                    tableModel.addRow(new Object[]{stock,
+                                    type, amount, "$" + price, date
+                            }
+                    );
+                }
+            }
         }
+
         table.setModel(tableModel);
         JTableHeader header = table.getTableHeader();
         header.setBackground(Color.LIGHT_GRAY);
