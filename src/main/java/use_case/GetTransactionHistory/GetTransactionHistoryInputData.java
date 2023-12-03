f35917069859d829f59516ea0727937c9d8537d3
Ricky Grosvenor-Pramanick <ronakpramanick@gmail.com>
2023-12-02 20:58:43 -0500

    Completed Testing for GetTransactionHistory Use Case

--- src/main/java/use_case/GetTransactionHistory/GetTransactionHistoryInputData.java
+++
@@ -1,9 +0,0 @@
-package use_case.GetTransactionHistory;
-
-public class GetTransactionHistoryInputData {
-    String filterBy = null;
-
-    public GetTransactionHistoryInputData(String filterBy) {
-        this.filterBy = filterBy;
-    }
-}
