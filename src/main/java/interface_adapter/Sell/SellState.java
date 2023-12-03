7894c930d129378ebd94222c233dcb2ec9d0ad27
gursi26 <gursimar.batra@gmail.com>
2023-12-03 16:07:16 -0500

    added javadoc for drivers, brought back unused constructor from SellState because it appears to be used in tests

--- src/main/java/interface_adapter/Sell/SellState.java
+++ src/main/java/interface_adapter/Sell/SellState.java
@@ -3,23 +3,47 @@ package interface_adapter.Sell;
 import java.util.List;
 
 public class SellState {
-    // contains the current selected stock in the dropdown menu
     private String stockSelected = null;
-    // contains the error message regarding the amount, either a format error
-    // or the user tried to sell more stocks than owned
     private String amountError = null;
-    // contains the amount of stocks the user wants to sell
     private String amount = null;
-    // contains the success message displayed upon selling stocks.
     private String sellSuccess = null;
-    // list containing stocks currently owned by the user
     private List<String> ownedStocks = null;
-    // list containing amounts of stocks currently owned by the user
     private List<Double> ownedAmounts = null;
-    // list containing the sell prices of the stocks owned by the user
     private List<Double> sellAmounts = null;
-    // contains the current balance of the user
     private Double balance = null;
+
+    /**
+     * constructor for the sell state
+     *
+     * @param stockSelected     contains the current selected stock in the dropdown menu
+     * @param amount            contains the amount of stocks the user wants to sell
+     * @param amountError       contains the error message regarding the amount, either a format error
+     *                          or the user tried to sell more stocks than owned
+     * @param ownedStocks       list containing stocks currently owned by the user
+     * @param sellSuccess       contains the success message displayed upon selling stocks.
+     * @param ownedAmounts      contains amounts of stocks owned by the user
+     * @param balance           contains the current balance of the user
+     * @param sellAmounts       list containing the sell prices of the stocks owned by the user
+     */
+    public SellState(
+            String stockSelected,
+            String amount,
+            String amountError,
+            List<String> ownedStocks,
+            String sellSuccess,
+            List<Double> ownedAmounts,
+            Double balance,
+            List<Double> sellAmounts
+    ) {
+        this.amountError = amountError;
+        this.amount = amount;
+        this.stockSelected = stockSelected;
+        this.ownedStocks = ownedStocks;
+        this.sellSuccess = sellSuccess;
+        this.ownedAmounts = ownedAmounts;
+        this.balance = balance;
+        this.sellAmounts = sellAmounts;
+    }
 
     public String getAmountError() {
         return amountError;
