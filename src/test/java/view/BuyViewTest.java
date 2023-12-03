31a810eeb06be5348e57727425270e81b8fc0ad1
Zain <zainhmoustafa@gmail.com>
2023-12-03 18:16:00 -0500

    added buy and sell view tests

---
+++ src/test/java/view/BuyViewTest.java
@@ -0,0 +1,157 @@
+package view;
+
+import driver.TableModel;
+import interface_adapter.Buy.BuyController;
+import interface_adapter.Buy.BuyState;
+import interface_adapter.Buy.BuyViewModel;
+import interface_adapter.Dashboard.DashboardViewModel;
+import interface_adapter.ViewManagerModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+
+public class BuyViewTest {
+
+    private BuyController buyController;
+    private BuyViewModel buyViewModel;
+    private DashboardViewModel dashboardViewModel;
+    private ViewManagerModel viewManagerModel;
+    private BuyView buyView;
+    private JFrame jf;
+
+    @BeforeEach
+    public void setUp() {
+        buyViewModel = new BuyViewModel();
+
+        DashboardViewModel dashboardViewModel = new DashboardViewModel();
+        buyController = Mockito.mock(BuyController.class);
+        viewManagerModel = Mockito.mock(ViewManagerModel.class);
+
+        buyView = new BuyView(buyController, buyViewModel, viewManagerModel, dashboardViewModel);
+
+        jf = new JFrame();
+        jf.setContentPane(buyView);
+        jf.pack();
+        jf.setVisible(true);
+    }
+
+    @Test
+    public void testSearchStockUpdatesView() {
+        Mockito.doAnswer(invocation -> {
+            HashMap<String, String> companyNews = new HashMap<>();
+            companyNews.put("country", "US");
+            companyNews.put("weburl", "https://www.apple.com/");
+            companyNews.put("currently held", "0.0");
+
+            buyViewModel.getState().setRenderNewInfo(true);
+            buyViewModel.getState().setStockInfo(companyNews);
+            buyViewModel.firePropertyChanged();
+            return null;
+        }).when(buyController).execute(any(String.class));
+
+        JTextField tickerInputField = buyView.tickerInputField;
+        tickerInputField.setText("AAPL");
+        buyViewModel.getState().setTicker("AAPL");
+
+        JButton searchButton = buyView.search;
+        searchButton.doClick();
+
+        Map<String, String> companyNews = buyViewModel.getState().getStockInfo();
+        assertEquals(companyNews.get("country"), "US");
+        assertEquals(companyNews.get("weburl"), "https://www.apple.com/");
+        assertEquals(companyNews.get("currently held"), "0.0");
+
+        TableModel tableModel = (TableModel) buyView.table.getModel();
+
+        assertEquals("US", tableModel.getValueAt(0, 1));
+        assertEquals("https://www.apple.com/", tableModel.getValueAt(1, 1));
+        assertEquals("0.0", tableModel.getValueAt(2, 1));
+
+        // Assuming the first row contains the new stock information
+//        int firstRow = 0;
+//        assertEquals("US", tableModel.getValueAt(firstRow, getColumnIndex(tableModel, "country")));
+//        assertEquals("https://www.apple.com/", tableModel.getValueAt(firstRow, getColumnIndex(tableModel, "weburl")));
+//        assertEquals("10.0", tableModel.getValueAt(firstRow, getColumnIndex(tableModel, "currently held")));
+
+        // Additional assertions as needed
+    }
+
+//    private int getColumnIndex(DefaultTableModel model, String columnName) {
+//        for (int i = 0; i < model.getColumnCount(); i++) {
+//            if (columnName.equals(model.getColumnName(i))) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+
+    @Test
+    public void testBuyStockTriggersControllerExecution() {
+        JTextField tickerInputField = buyView.tickerInputField;
+        JTextField amountInputField = buyView.amountInputField;
+        tickerInputField.setText("AAPL");
+        amountInputField.setText("10");
+
+        BuyState currentState = buyViewModel.getState();
+        currentState.setAmount("10");
+        currentState.setTicker("AAPL");
+        buyViewModel.setState(currentState);
+
+        JButton purchaseButton = buyView.purchase;
+        purchaseButton.doClick();
+
+        Mockito.verify(buyController, Mockito.times(1)).execute(eq("10"), eq("AAPL"));
+    }
+
+
+    @Test
+    public void testBackButtonNavigatesToDashboard() {
+        JButton backButton = findButton(buyView, "Back");
+        assertNotNull(backButton);
+
+        backButton.doClick();
+        Mockito.verify(viewManagerModel, Mockito.times(1)).setActiveView("dashboard");
+    }
+
+    private JButton findButton(Component component, String query) {
+        if (component instanceof JButton) {
+            JButton button = (JButton) component;
+            if (query.equals(button.getText())) {
+                return button;
+            }
+        }
+        if (component instanceof Container) {
+            for (Component child : ((Container) component).getComponents()) {
+                JButton button = findButton(child, query);
+                if (button != null) {
+                    return button;
+                }
+            }
+        }
+        return null;
+    }
+    private Component findComponentByName(Component component, String name) {
+        if (name.equals(component.getName())) {
+            return component;
+        }
+        if (component instanceof Container) {
+            for (Component child : ((Container) component).getComponents()) {
+                Component found = findComponentByName(child, name);
+                if (found != null) {
+                    return found;
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/view/SellViewTest.java
@@ -0,0 +1,157 @@
+package view;
+
+import data_access.InMemoryUserDataAccessObject;
+import entity.*;
+import interface_adapter.Dashboard.DashboardViewModel;
+import interface_adapter.Sell.SellController;
+import interface_adapter.Sell.SellState;
+import interface_adapter.Sell.SellViewModel;
+import interface_adapter.ViewManagerModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SellViewTest {
+
+    private SellController sellController;
+    private SellViewModel sellViewModel;
+    private DashboardViewModel dashboardViewModel;
+    private ViewManagerModel viewManagerModel;
+    private SellView sellView;
+    private JFrame jf;
+
+    @BeforeEach
+    public void setUp() {
+        InMemoryUserDataAccessObject inMemoryUserDataAccessObject = new InMemoryUserDataAccessObject(new CommonUserFactory());
+        User user = inMemoryUserDataAccessObject.get();
+        user.addToPortfolio("AAPL", 10.0);
+
+        List<Transaction> transactionList = new ArrayList<>();
+        transactionList.add(new BuyTransaction(10.0, new PricePoint(LocalDateTime.now(), 100.0)));
+
+        user.getHistory().put("AAPL", new TransactionHistory(
+                new Stock(100.0, "Apple Inc", "AAPL"),
+                transactionList
+        ));
+
+        List<String> ownedStocks = new ArrayList<>();
+        ownedStocks.add("AAPL");
+        List<Double> ownedAmounts = new ArrayList<>();
+        ownedAmounts.add(10.0);
+        List<Double> sellAmounts = new ArrayList<>();
+        sellAmounts.add(100.0);
+
+        sellController = Mockito.mock(SellController.class);
+        SellState sellState = new SellState(
+                "AAPL", "5", null, ownedStocks,
+                null, ownedAmounts, 10000.0, sellAmounts);
+
+        sellViewModel = new SellViewModel();
+        sellViewModel.setState(sellState);
+        DashboardViewModel dashboardViewModel = new DashboardViewModel();
+//        dashboardViewModel = Mockito.mock(DashboardViewModel.class);
+        viewManagerModel = Mockito.mock(ViewManagerModel.class);
+
+        sellView = new SellView(sellViewModel, sellController, viewManagerModel, dashboardViewModel);
+
+        jf = new JFrame();
+        jf.setContentPane(sellView);
+        jf.pack();
+        jf.setVisible(true);
+    }
+
+    private JButton findButton(Component component, String query) {
+        if (component instanceof JButton) {
+            JButton button = (JButton) component;
+            if (query.equals(button.getText())) {
+                return button;
+            }
+        }
+        if (component instanceof Container) {
+            for (Component child : ((Container) component).getComponents()) {
+                JButton button = findButton(child, query);
+                if (button != null) {
+                    return button;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Test
+    public void testSellViewBehavior() {
+        JButton sellButton = findButton(sellView, "Sell Stocks");
+        assertNotNull(sellButton);
+        sellButton.doClick();
+
+        // Use Mockito to verify interactions
+        Mockito.verify(sellController, Mockito.times(1)).execute();
+
+        // Additional tests can include testing property change listeners,
+        // key event handling, action events, etc.
+
+        // Add more assertions and verifications as needed for your test
+    }
+
+    @Test
+    public void testStockSelectionUpdatesSellState() {
+        JComboBox<String> stockInputField = (JComboBox<String>) findComponentByName(sellView, "stockInputField");
+        assertNotNull(stockInputField);
+
+        stockInputField.setSelectedItem("AAPL");
+        assertEquals("AAPL", sellViewModel.getState().getStockSelected());
+    }
+
+    @Test
+    public void testSellAmountInputUpdatesSellState() {
+        JTextField amountInputField = (JTextField) findComponentByName(sellView, "amountInputField");
+        assertNotNull(amountInputField);
+
+        amountInputField.setText("5");
+        assertEquals("5", sellViewModel.getState().getAmount());
+    }
+
+    @Test
+    public void testPropertyChangeUpdatesView() {
+        sellViewModel.getState().setBalance(5000.0);
+        sellViewModel.firePropertyChanged();
+
+        JLabel currentBalanceLabel = (JLabel) findComponentByName(sellView, "currentBalance");
+        assertNotNull(currentBalanceLabel);
+
+        assertEquals("Current Balance: $5000.00", currentBalanceLabel.getText());
+    }
+
+    @Test
+    public void testBackButtonNavigatesToDashboard() {
+        JButton backButton = findButton(sellView, "Back");
+        assertNotNull(backButton);
+
+        backButton.doClick();
+        Mockito.verify(viewManagerModel, Mockito.times(1)).setActiveView("dashboard");
+    }
+
+    private Component findComponentByName(Component component, String name) {
+        if (name.equals(component.getName())) {
+            return component;
+        }
+        if (component instanceof Container) {
+            for (Component child : ((Container) component).getComponents()) {
+                Component found = findComponentByName(child, name);
+                if (found != null) {
+                    return found;
+                }
+            }
+        }
+        return null;
+    }
+}
